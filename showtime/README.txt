3b92530dc430060bdef28ea761be92444a421b28
Chloe Spilker <chloe.spilker@tooeleschools.org>
2019-08-30 09:33:45 -0600

    yeet

---
+++ showtime/README.txt
@@ -0,0 +1,6 @@
+cmd prompt
+
+set path="C:\Program Files\Java\jdk-12.0.2\bin"
+cd "Desktop\c.spilker\showtime"
+javac ShowTime.java
+java ShowTime
---
+++ showtime/ShowTime.java
@@ -0,0 +1,11 @@
+import java.util.Calendar;
+import static java.lang.System.out;
+
+public class ShowTime {
+
+	public static void main(String[] args) {
+		out.println(Calendar.getInstance().getTime());
+
+	}
+
+}
